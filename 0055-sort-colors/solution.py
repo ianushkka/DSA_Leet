88c3a912327bab7383a813da8916458ea00d5261
Anushka <imanushkaa@gmail.com>
2026-01-24 23:16:21 +0530

    Adding solution 55

---
+++ 0055-sort-colors/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None: 
+        start=0 
+        for i in range(len(nums)) :
+            if nums[i]==0 :
+                nums[start],nums[i]=nums[i],nums[start]  
+                start+=1 
+
+        #start points to the index next to last zero 
+        #so count of zeroes is exactly as start basically 
+
+        for i in range(start,len(nums)) : 
+            if nums[i]==1 :
+                nums[start],nums[i]=nums[i],nums[start]  
+                start+=1 
+             
+
+        
