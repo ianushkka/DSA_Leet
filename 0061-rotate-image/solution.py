3996a4f3cd1ee13714feb81713190c187cb93369
Anushka <imanushkaa@gmail.com>
2026-02-01 20:24:35 +0530

    Adding solution 61

---
+++ 0061-rotate-image/solution.py
@@ -0,0 +1,19 @@
+from typing import List 
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """  
+
+        for i in range(len(matrix))  :
+            for j in range(len(matrix[0])) : 
+                if j>i :
+                    matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]   
+
+
+        for nums in matrix : 
+            nums.reverse()
+
+
+
+         
